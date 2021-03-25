e7b33f9ebf807d30a95563304e175ab862e7075a
Hari Kammana <hari.kammana@gmail.com>
2021-03-25 09:29:50 +0530

    dynamic blocks

--- web-servers-ec2.tf
+++ web-servers-ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  ami           = var.web_ami
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.*.id[0]
-  key_name = "javahome"
+  ami                    = var.web_ami
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.*.id[0]
+  key_name               = "javahome"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  user_data = file("./files/apache.sh")
-  iam_instance_profile = aws_iam_instance_profile.web_s3_profile.name
+  user_data              = file("./files/apache.sh")
+  iam_instance_profile   = aws_iam_instance_profile.web_s3_profile.name
   tags = {
     Name = "HelloWorld-${local.ws}"
   }
@@ -25,7 +25,7 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   ingress {
+  ingress {
     description = "ssh for web servers"
     from_port   = 22
     to_port     = 22
